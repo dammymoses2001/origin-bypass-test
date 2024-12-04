cb5c563827e785ec16d0b2355c7835953c6eb9ca
Dammy moses <61425976+dammymoses2001@users.noreply.github.com>
2024-12-04 11:08:39 +0100

    update

--- src/pages/origin1.js
+++ src/pages/origin1.js
@@ -5,6 +5,19 @@ export default function Origin1() {
     <div>
       <div className='text-center text-3xl mt-3'>this is different origin without access</div>
       <iframe src="https://form-iframer.vercel.app/dashboard" title="Example iframe" frameBorder="0" width="100%" height="700"></iframe>
+      <form>
+  <label>
+    Name:
+    <input type="text" name="name" />
+  </label>
+  <br />
+  <label>
+    Email:
+    <input type="email" name="email" />
+  </label>
+  <br />
+  <input type="submit" value="Submit" />
+</form>
     </div>
   )
 }
